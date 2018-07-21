24d7409f3f5e83e8d14f8f7b2b3e2a0fa150ce53
Cesar Richard <cesar.richard@u-psud.fr>
2018-07-21 15:47:58 +0200

    Lint + update
    Signed-off-by: Cesar Richard <cesar.richard@u-psud.fr>

--- class/config.js
+++ class/config.js
@@ -3,24 +3,18 @@ const mysql = require('mysql');
 module.exports = {
   debug: true,
 
-  facebook: {
-    api_key: '1796577050626136',
-    api_secret: 'adc0b450fea5da242a6e4a7aa6802eba',
-    callback_url: 'http://node.crichard.fr/auth/facebook/callback'
-  },
-
   cas: {
     cas_url: 'https://cas.utc.fr/cas',
-    service_url: 'http://node.crichard.fr/',
+    service_url: process.env.url || 'http://node.crichard.fr/',
     cas_version: '2.0',
     session_info: 'cas_infos'
   },
 
   database: {
     host: 'localhost',
-    username: 'parserfb',
-    password: 'parserfb',
-    database: 'parserfb'
+    username: process.env.sqluser || 'parserfb',
+    password: process.env.sqlpass || 'parserfb',
+    database: process.env.sqlbase || 'parserfb'
   },
 
   listenPort: 3651,
@@ -31,7 +25,7 @@ module.exports = {
     };
     Array.prototype.findObjectByProp = function _findObjectByProp(prop, value) {
       for (let i = 0; i < this.length; i++) {
-        if (this[i][prop] == value) {
+        if (this[i][prop] === value) {
           return this[i];
         }
       }
@@ -40,7 +34,7 @@ module.exports = {
 
     Array.prototype.extend = function _extendArray(array) {
       if (typeof array === 'undefined') throw new Error('Argument 1 (and only) must be an array');
-      this.push.apply(this, array);
+      this.push.apply(this, array); // eslint-disable-line prefer-spread
     };
     const pool = mysql.createPool({
       connectionLimit: 50,
