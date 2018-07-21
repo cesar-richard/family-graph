38ee1efebf8f2a37f927ac6685bbb5f1de2e996a
Cesar Richard <cesar.richard@u-psud.fr>
2018-07-22 00:50:54 +0200

    Added test
    Signed-off-by: Cesar Richard <cesar.richard@u-psud.fr>

--- test/index.js
+++ test/index.js
@@ -3,23 +3,74 @@ const chai = require('chai'),
   server = require('./../app'),
   should = chai.should();
 
-describe('Home', () => {
-  describe('HTML', () => {
-    describe('/', () => {
-      it('should return home page', done => {
-        chai
-          .request(server)
-          .get('/')
-          .then(res => {
-            res.should.have.status(200);
-            res.should.be.html;
-            dictum.chai(res);
-          })
-          .then(() => done())
-          .catch(err => {
-            done(new Error(err));
-          });
-      });
+describe('HTML', () => {
+  describe('/', () => {
+    it('should return home page', done => {
+      chai
+        .request(server)
+        .get('/')
+        .then(res => {
+          res.should.have.status(200);
+          res.should.be.html;
+          dictum.chai(res);
+        })
+        .then(() => done())
+        .catch(err => {
+          done(new Error(err));
+        });
     });
   });
+
+  describe('/view', () => {
+    it('should return graph page', done => {
+      chai
+        .request(server)
+        .get('/view')
+        .then(res => {
+          res.should.have.status(200);
+          res.should.be.html;
+          dictum.chai(res);
+        })
+        .then(() => done())
+        .catch(err => {
+          done(new Error(err));
+        });
+    });
+  });
+});
+
+describe('API', () => {
+   describe('/nodes', () => {
+it('should return nodes list', done => {
+      chai
+        .request(server)
+        .get('/nodes')
+        .then(res => {
+          res.should.have.status(200);
+          res.should.be.json;
+          dictum.chai(res);
+        })
+        .then(() => done())
+        .catch(err => {
+          done(new Error(err));
+        });
+    });
+   });
+
+   describe('/edges', () => {
+it('should return edges list', done => {
+      chai
+        .request(server)
+        .get('/edges')
+        .then(res => {
+          res.should.have.status(200);
+          res.should.be.json;
+          dictum.chai(res);
+        })
+        .then(() => done())
+        .catch(err => {
+          done(new Error(err));
+        });
+    });
+   });
 });
