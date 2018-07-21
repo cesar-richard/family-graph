987b4f5a286e433a742adc5cab9dd03f85019593
Cesar Richard <cesar.richard@u-psud.fr>
2018-07-22 00:40:51 +0200

    Added Unit Tests Signed-off-by: Cesar Richard <cesar.richard@u-psud.fr>

---
+++ test/index.js
@@ -0,0 +1,76 @@
+const chai = require('chai'),
+  dictum = require('dictum.js'),
+  server = require('./../app'),
+  should = chai.should();
+
+describe('HTML', () => {
+  describe('/', () => {
+    it('should return home page', done => {
+      chai
+        .request(server)
+        .get('/')
+        .then(res => {
+          res.should.have.status(200);
+          res.should.be.html;
+          dictum.chai(res);
+        })
+        .then(() => done())
+        .catch(err => {
+          done(new Error(err));
+        });
+    });
+  });
+
+  describe('/view', () => {
+    it('should return graph page', done => {
+      chai
+        .request(server)
+        .get('/view')
+        .then(res => {
+          res.should.have.status(200);
+          res.should.be.html;
+          dictum.chai(res);
+        })
+        .then(() => done())
+        .catch(err => {
+          done(new Error(err));
+        });
+    });
+  });
+});
+
+describe('API', () => {
+   describe('/nodes', () => {
+it('should return nodes list', done => {
+      chai
+        .request(server)
+        .get('/nodes')
+        .then(res => {
+          res.should.have.status(200);
+          res.should.be.json;
+          dictum.chai(res);
+        })
+        .then(() => done())
+        .catch(err => {
+          done(new Error(err));
+        });
+    });
+   });
+
+   describe('/edges', () => {
+it('should return edges list', done => {
+      chai
+        .request(server)
+        .get('/edges')
+        .then(res => {
+          res.should.have.status(200);
+          res.should.be.json;
+          dictum.chai(res);
+        })
+        .then(() => done())
+        .catch(err => {
+          done(new Error(err));
+        });
+    });
+   });
+});
