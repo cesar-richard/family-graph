f57bb89595e2554295f8c465a90733308fbf12b0
Cesar Richard <cesar.richard@u-psud.fr>
2018-07-22 17:16:06 +0200

    Sequelize to be tested
    Signed-off-by: Cesar Richard <cesar.richard@u-psud.fr>

--- config/index.js
+++ config/index.js
@@ -29,7 +29,7 @@ const config = {
   common: {
     port: 3651,
     database: {
-      url: process.env.NODE_API_DB_URL,
+      //url: process.env.NODE_API_DB_URL,
       host: process.env.NODE_API_DB_HOST,
       port: process.env.NODE_API_DB_PORT,
       name: process.env.NODE_API_DB_NAME,
