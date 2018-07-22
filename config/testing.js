d48a33a5c37170bbb91fa4c01f9aa49b4e5a0734
Cesar Richard <cesar.richard@u-psud.fr>
2018-07-22 17:18:57 +0200

    Sequelize to be tested
    Signed-off-by: Cesar Richard <cesar.richard@u-psud.fr>

--- config/testing.js
+++ config/testing.js
@@ -5,7 +5,7 @@ exports.config = {
     port: 8081,
     database: {
       name: process.env.MYSQL_DATABASE,
-      host: process.env.sqlhost,
+      host: process.env.sqlhost||'mysql',
       username: process.env.sqluser,
       password: process.env.MYSQL_ROOT_PASSWORD
     },
