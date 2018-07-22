4685def0498c341504e04ed346fecba5b020c091
Cesar Richard <cesar.richard@u-psud.fr>
2018-07-22 18:06:12 +0200

    Tests pipeline checkup
    Signed-off-by: Cesar Richard <cesar.richard@u-psud.fr>

--- app/models/nodes.js
+++ app/models/nodes.js
@@ -2,11 +2,10 @@ const Sequelize = require('sequelize');
 
 exports.getModel = db => {
   const nodes = db.define('nodes', {
-      label: Sequelize.STRING,
-      x: Sequelize.FLOAT,
-      y: Sequelize.FLOAT
+    label: Sequelize.STRING,
+    x: Sequelize.FLOAT,
+    y: Sequelize.FLOAT
   });
-  nodes.associate = function(models) {
-  };
+  nodes.associate = function(models) {};
   return nodes;
 };
