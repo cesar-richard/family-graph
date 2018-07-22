e5a5a753bd5c7ebcb52ba3ba90e36bcc62974e6d
Cesar Richard <cesar.richard@u-psud.fr>
2018-07-22 17:46:39 +0200

    Sequelize to be tested
    Signed-off-by: Cesar Richard <cesar.richard@u-psud.fr>

--- migrations/config.js
+++ migrations/config.js
@@ -1,8 +1,5 @@
 const config = require('./../config');
-console.log({
-  [config.environment]: Object.assign({}, config.database, {
-    dialect: 'mysql'
-  }));
+
 module.exports = {
   [config.environment]: Object.assign({}, config.database, {
     dialect: 'mysql'
