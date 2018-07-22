7de3a7ce7e51689f379d095b82bc426c792dffa4
Cesar Richard <cesar.richard@u-psud.fr>
2018-07-22 13:49:15 +0200

    add sequelize
    Signed-off-by: Cesar Richard <cesar.richard@u-psud.fr>

---
+++ migrations/migrations/1-create-edges.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('edges', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      from: {
+        type: Sequelize.INTEGER
+      },
+      to: {
+        type: Sequelize.INTEGER
+      },
+      arrows: {
+        type: Sequelize.STRING
+      },
+      dashes: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('associations');
+  }
+};
---
+++ migrations/migrations/1-create-nodes.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('nodes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      when: {
+        type: Sequelize.DATE
+      },
+      route: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('users');
+  }
+};
