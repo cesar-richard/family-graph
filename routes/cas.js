90d6c8fa4e341c4eeea0b923cf2d71d03ec98447
César Richard <cesar.richard@hds.utc.fr>
2018-07-26 13:03:12 +0200

    Deleted unused cas.js file

--- routes/cas.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-/* GET CAS Path */
-router.get('/authenticate', global.cas.bounce);
-router.get('/logout', global.cas.logout);
-
-module.exports = router;
